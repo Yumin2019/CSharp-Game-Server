a62db61e22394fb285942134c7cbb878bb8f5fd3
Yumin Kim <yumingames@gmail.com>
2023-08-19 23:09:12 +0900

    feat: Connector(소켓 연결용) 클래스 추가 feat: DummyClient, Server에서 ServerCore.dll을 참조하도록 수정 change: DummyClient, Server 코드를 ServerCore 인터페이스를 사용하도록 변경

--- DummyClient/Program.cs
+++ DummyClient/Program.cs
@@ -1,10 +1,42 @@
-﻿using System;
+﻿using ServerCore;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
 namespace DummyClient
 {
+
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine("OnConnected  endPoint from server = " + endPoint);
+
+            for (int i = 0; i < 5; ++i)
+            {
+                // 보낸다. 
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World " + i);
+                Send(sendBuff);
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine("OnDisconnected  endPoint  " + endPoint);
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine("recvData from server = " + recvData);
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine("transperedBytes = " + numOfBytes);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
@@ -15,33 +47,14 @@ namespace DummyClient
             IPAddress ipAddr = ipHost.AddressList[0];
             IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            while(true)
+            Connector connector = new Connector();
+            connector.Connect(endPoint, () => { return new GameSession(); });
+
+            while (true)
             {
                 try
                 {
-                    // 휴대폰 설정
-                    Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
-                    // 문지기 한테 입장 문의
-                    socket.Connect(endPoint);
-                    Console.WriteLine("connected to socket " + socket.RemoteEndPoint.ToString());
-
-                    for (int i = 0; i < 5; ++i)
-                    {
-                        // 보낸다. 
-                        byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World " + i);
-                        int sendBytes = socket.Send(sendBuff);
-                    }
-
-                    // 받는다.
-                    byte[] recvBuff = new byte[1024];
-                    int recvBytes = socket.Receive(recvBuff);
-                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
-                    Console.WriteLine("recvData = " + recvData);
-
-                    // 나간다.
-                    socket.Shutdown(SocketShutdown.Both);
-                    socket.Close();
                 }
                 catch (Exception e)
                 {
