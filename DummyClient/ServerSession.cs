6e0db440d9b69c0ac5a0210764c30a26ba7f9107
Yumin Kim <yumingames@gmail.com>
2023-08-22 21:54:08 +0900

    change: PacketGenerator 코드 수정

--- DummyClient/ServerSession.cs
+++ DummyClient/ServerSession.cs
@@ -13,17 +13,25 @@ namespace DummyClient
     {
         public long playerId;
         public string name;
-
-        public struct SkillInfo
+        public struct Skill
         {
             public int id;
             public short level;
             public float duration;
 
+            public void Read(ReadOnlySpan<byte> s, ref ushort count)
+            {
+                this.id = BitConverter.ToInt32(s.Slice(count, s.Length - count));
+                count += sizeof(int);
+                this.level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
+                count += sizeof(short);
+                this.duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
+                count += sizeof(float);
+            }
+
             public bool Write(Span<byte> s, ref ushort count)
             {
                 bool success = true;
-
                 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), id);
                 count += sizeof(int);
                 success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), level);
@@ -32,89 +40,53 @@ namespace DummyClient
                 count += sizeof(float);
                 return success;
             }
-
-            public void Read(ReadOnlySpan<byte> s, ref ushort count)
-            {
-                id =  BitConverter.ToInt32(s.Slice(count, s.Length - count));
-                count += sizeof(int);
-                level = BitConverter.ToInt16(s.Slice(count, s.Length - count));
-                count += sizeof(short);
-                duration = BitConverter.ToSingle(s.Slice(count, s.Length - count));
-                count += sizeof(float);
-
-            }
         }
 
-        public List<SkillInfo> skills = new List<SkillInfo>();
-
-        public PlayerInfoReq()
-        {
-        }
-
+        public List<Skill> skill = new List<Skill>();
         public void Read(ArraySegment<byte> segment)
         {
             ushort count = 0;
-
             ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
 
             count += sizeof(ushort);
             count += sizeof(ushort);
-
             this.playerId = BitConverter.ToInt64(s.Slice(count, s.Length - count));
             count += sizeof(long);
-
             ushort nameLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += sizeof(ushort);
-
             this.name = Encoding.Unicode.GetString(s.Slice(count, nameLen));
             count += nameLen;
-
-            // skill list
-            skills.Clear();
+            skill.Clear();
             ushort skillLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
             count += sizeof(ushort);
             for (int i = 0; i < skillLen; ++i)
             {
-                SkillInfo skillInfo = new SkillInfo();
-                skillInfo.Read(s, ref count);
-                skills.Add(skillInfo);
+                Skill skill = new Skill();
+                skill.Read(s, ref count);
+                this.skill.Add(skill);
             }
         }
 
-        public  ArraySegment<byte> Write()
+        public ArraySegment<byte> Write()
         {
             ArraySegment<byte> segment = SendBufferHelper.Open(4096);
-
             ushort count = 0;
             bool success = true;
 
             Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
-
             count += sizeof(ushort);
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.PlayerInfoReq);
             count += sizeof(ushort);
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), playerId);
             count += sizeof(long);
-
-            //ushort nameLen = (ushort)Encoding.Unicode.GetByteCount(this.name);
-            //success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
-            //count += sizeof(ushort);
-            //Array.Copy(Encoding.Unicode.GetBytes(this.name), 0, segment.Array, segment.Offset + count, nameLen);
-            //count += nameLen;
-
-            // 이미지 같은 것도 이런 식으로 처리한다. 
-            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0,
-                this.name.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+            ushort nameLen = (ushort)Encoding.Unicode.GetBytes(this.name, 0, this.name.Length, segment.Array, segment.Offset + count + sizeof(ushort));
             success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), nameLen);
             count += sizeof(ushort);
             count += nameLen;
-
-            // skill list
-            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)skills.Count);
+            success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)this.skill.Count);
             count += sizeof(ushort);
-            foreach (SkillInfo skill in skills)
+            foreach (Skill skill in this.skill)
                 success &= skill.Write(s, ref count);
-
             success &= BitConverter.TryWriteBytes(s, count);
 
             if (success == false)
@@ -144,9 +116,9 @@ namespace DummyClient
             
             PlayerInfoReq packet = new PlayerInfoReq() { playerId = 1001,
                 name = "ABCD" };
-            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 101, level = 1, duration = 3.0f });
-            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 102, level = 2, duration = 4.0f });
-            packet.skills.Add(new PlayerInfoReq.SkillInfo() { id = 103, level = 3, duration = 5.0f });
+            packet.skill.Add(new PlayerInfoReq.Skill() { id = 101, level = 1, duration = 3.0f });
+            packet.skill.Add(new PlayerInfoReq.Skill() { id = 102, level = 2, duration = 4.0f });
+            packet.skill.Add(new PlayerInfoReq.Skill() { id = 103, level = 3, duration = 5.0f });
 
 
             {
--- PacketGenerator/Program.cs
+++ PacketGenerator/Program.cs
@@ -5,6 +5,8 @@ namespace PacketGenerator
 {
     class Program
     {
+        static string genPackets = "";
+
         static void Main(string[] args)
         {
             XmlReaderSettings settings = new XmlReaderSettings()
@@ -24,6 +26,8 @@ namespace PacketGenerator
                     Console.WriteLine("name = " + r.Name + " " + r["name"]);
                 }
             }
+
+            File.WriteAllText("GenPackets.cs", genPackets);
         }
 
         public static void ParsePacket(XmlReader r)
@@ -44,12 +48,20 @@ namespace PacketGenerator
                 return;
             }
 
-            ParseMember(r);
+            var tuple3 = ParseMember(r);
+            genPackets += string.Format(PacketFormat.packetFormat, packetName, tuple3.Item1, tuple3.Item2, tuple3.Item3);
         }
 
-        private static void ParseMember(XmlReader r)
+        // {1} 멤버 변수들
+        // {2} 멤버변수 Read
+        // {3} 멤버변수 Write
+        private static Tuple<string, string, string> ParseMember(XmlReader r)
         {
             string packetName = r["name"];
+            string memberCode = "";
+            string readCode = "";
+            string writeCode = "";
+
             int depth = r.Depth + 1;
             while(r.Read())
             {
@@ -61,8 +73,15 @@ namespace PacketGenerator
                 if(string.IsNullOrEmpty(memberName))
                 {
                     Console.WriteLine("member without name");
-                    return;
+                    return null;
                 }
+
+                if(string.IsNullOrEmpty(memberCode) == false)
+                    memberCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(readCode) == false)
+                    readCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(writeCode) == false)
+                    writeCode += Environment.NewLine;
 
                 string memberType = r.Name.ToLower();
                 switch(memberType)
@@ -75,11 +94,98 @@ namespace PacketGenerator
                     case "long":
                     case "float":
                     case "double":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readFormat, memberName, ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeFormat, memberName, memberType);
+                        break;
                     case "string":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readStringFormat, memberName);
+                        writeCode += string.Format(PacketFormat.writeStringFormat, memberName);
+                        break;
                     case "list":
+                        Tuple<string, string, string> tuple = ParseList(r);
+                        memberCode += tuple.Item1;
+                        readCode += tuple.Item2;
+                        writeCode += tuple.Item3;
                         break;
                 }
             }
+
+            memberCode = memberCode.Replace("\n", "\n\t");
+            readCode = readCode.Replace("\n", "\n\t\t");
+            writeCode = writeCode.Replace("\n", "\n\t\t");
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static Tuple<string, string, string> ParseList(XmlReader r)
+        {
+            string listName = r["name"];
+            if(string.IsNullOrEmpty(listName))
+            {
+                Console.WriteLine("List without name");
+                return null;
+            }
+
+            Tuple<string, string, string> tuple3 = ParseMember(r);
+            string memberCode = string.Format(PacketFormat.memberListFormat, 
+                FirstCharToUpper(listName), 
+                FirstCharToLower(listName), 
+                tuple3.Item1, 
+                tuple3.Item2, 
+                tuple3.Item3);
+
+            string readCode = string.Format(PacketFormat.readListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            string writeCode = string.Format(PacketFormat.writeListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static string ToMemberType(string memberType)
+        {
+            switch (memberType)
+            {
+                case "bool":
+                    return "ToBoolean";
+                case "short":
+                    return "ToInt16";
+                case "ushort":
+                    return "ToUInt16";
+                case "int":
+                    return "ToInt32";
+                case "long":
+                    return "ToInt64";
+                case "float":
+                    return "ToSingle";
+                case "double":
+                    return "ToDouble";
+                case "string":
+                    break;
+                case "list":
+                    break;
+            }
+
+            return "";
+        }
+       
+        public static string FirstCharToUpper(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToUpper() + input.Substring(1);
+        }
+
+        public static string FirstCharToLower(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return "";
+            return input[0].ToString().ToLower() + input.Substring(1);
         }
     }
+
 }
